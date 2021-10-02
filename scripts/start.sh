2e66d60e7f396f6504fb6aeadc37b9cad3200904
DCsunset <DCsunset@users.noreply.github.com>
2021-10-01 20:26:54 -0400

    fix: fix ownership

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,23 +5,34 @@ set -e
 # Run init.sh if it exists
 [[ -f /scripts/init.sh ]] && /scripts/init.sh && rm /scripts/init.sh
 
+# current user
+CUSER=${USERNAME:-root}
 # add user if specified
-if [ ! -z $USERNAME ]; then
-	pacman -S --noconfirm sudo
-	useradd -m -G wheel $USERNAME
-	sed -i "s/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/g" /etc/sudoers
+if [ ! -z $USERNAME ] ; then
+	HOMEDIR="/home/$USERNAME"
+	if [ ! -d "$HOMEDIR" ]; then
+		pacman -S --noconfirm sudo
+		useradd -m -G wheel $USERNAME
+		sed -i "s/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/g" /etc/sudoers
+	fi
+else
+	HOMEDIR="/root"
 fi
 
+
+
 umask 0077                # use safe default permissions
-mkdir -p "$HOME/.vnc"
-chmod go-rwx "$HOME/.vnc" # enforce safe permissions
+mkdir -p "$HOMEDIR/.vnc"
+chmod go-rwx "$HOMEDIR/.vnc" # enforce safe permissions
 
 # Start TigerVNC
 if [ ! -z $VNC_PASSWD ]; then
-	vncpasswd -f <<< "$VNC_PASSWD" > "$HOME/.vnc/passwd"
+	vncpasswd -f <<< "$VNC_PASSWD" > "$HOMEDIR/.vnc/passwd"
 fi
 
-vncsession ${USERNAME:-root} :0
+chown -R $CUSER "$HOMEDIR/.vnc"
+
+vncsession $CUSER :0
 
 # Start noVNC
 /noVNC/utils/launch.sh
